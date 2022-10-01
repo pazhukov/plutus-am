1a6597ecd77bc6c25ee92b90f45a30121a8cf661
Maxim Pazhukov <mpazhukov@gmail.com>
2022-10-01 09:09:00 +0300

    depo/bank accounts

--- account.go
+++ account.go
@@ -1,6 +1,12 @@
 package plutus
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 type DepoAccount struct {
 	ID     int    `json:"id"`
@@ -11,12 +17,12 @@ type DepoAccount struct {
 }
 
 type BankAccount struct {
-	ID           int    `json:"id"`
-	Owner        int    `json:"owner_id"`
-	Title        string `json:"title"`
-	CurrencyRate int    `json:"currency"`
-	Code         string `json:"code"`
-	Broker       string `json:"broker"`
+	ID       int    `json:"id"`
+	Owner    int    `json:"owner_id"`
+	Title    string `json:"title"`
+	Currency int    `json:"currency"`
+	Code     string `json:"code"`
+	Broker   string `json:"broker"`
 }
 
 type OwnerAccounts struct {
@@ -28,44 +34,446 @@ type OwnerAccounts struct {
 func NewDepoAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
 
+	var input DepoAccount
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = err.Error()
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Owner == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Owner can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Title == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Title can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Code == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Code can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Broker == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Broker can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	ownerID, status := DepoAccountNewItem(input)
+
+	if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = "New Depo Account with ID = " + strconv.Itoa(ownerID) + " added"
+		json.NewEncoder(w).Encode(info)
+	} else if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	}
+
 }
 
 func UpdateDepoAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
+
+	var input DepoAccount
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = err.Error()
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.ID == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account ID can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Owner == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Owner can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Title == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Title can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Code == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Code can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Broker == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Broker can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	status := DepoAccountUpdateItem(input)
+
+	if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = " Depo Account with ID = " + strconv.Itoa(input.ID) + " updated"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = " Depo Account with ID = " + strconv.Itoa(input.ID) + " not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	}
 
 }
 
 func GetDepoAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
 
+	inputVar := mux.Vars(r)["id"]
+	depoAccountID, err := strconv.Atoi(inputVar)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = "Can't convert Depo Account ID to int"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	item, status := DepoAccountGetItem(depoAccountID)
+
+	if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 404
+		info.Message = "Depo Account not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 200 {
+		json.NewEncoder(w).Encode(item)
+	}
+
 }
 
 func DeleteDepoAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
+
+	inputVar := mux.Vars(r)["id"]
+	depoAccountID, err := strconv.Atoi(inputVar)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = "Can't convert Depo Account ID to int"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	status := DepoAccountDeleteItem(depoAccountID)
+
+	if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 404
+		info.Message = "Depo Account not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = "Depo Account deleted"
+		json.NewEncoder(w).Encode(info)
+	}
 
 }
 
 func NewBankAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
 
+	var input BankAccount
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = err.Error()
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Owner == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Owner can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Title == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Title can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Currency == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Currency can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Code == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Code can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Broker == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Depo Account Broker can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	ownerID, status := BankAccountNewItem(input)
+
+	if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = "New Bank Account with ID = " + strconv.Itoa(ownerID) + " added"
+		json.NewEncoder(w).Encode(info)
+	} else if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	}
+
 }
 
 func UpdateBankAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
+
+	var input BankAccount
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = err.Error()
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.ID == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account ID can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Owner == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Owner can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Title == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Title can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Currency == 0 {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Currency can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Code == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Code can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	if input.Broker == "" {
+		var info InfoMessage
+		info.Code = 301
+		info.Message = "Bank Account Broker can't be empty"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	status := BankAccountUpdateItem(input)
+
+	if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = " Bank Account with ID = " + strconv.Itoa(input.ID) + " updated"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = " Bank Account with ID = " + strconv.Itoa(input.ID) + " not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	}
 
 }
 
 func GetBankAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
 
+	inputVar := mux.Vars(r)["id"]
+	bankAccountID, err := strconv.Atoi(inputVar)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = "Can't convert Bank Account ID to int"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	item, status := BankAccountGetItem(bankAccountID)
+
+	if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 404
+		info.Message = "Bank Account not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 200 {
+		json.NewEncoder(w).Encode(item)
+	}
+
 }
 
 func DeleteBankAccount(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
 
+	inputVar := mux.Vars(r)["id"]
+	bankAccountID, err := strconv.Atoi(inputVar)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = "Can't convert Bank Account ID to int"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	status := BankAccountDeleteItem(bankAccountID)
+
+	if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 404
+		info.Message = "Bank Account not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 200 {
+		var info InfoMessage
+		info.Code = 200
+		info.Message = "Bank Account deleted"
+		json.NewEncoder(w).Encode(info)
+	}
+
 }
 
 func GetAccountsByOwner(w http.ResponseWriter, r *http.Request) {
 	SetupCORS(&w, r)
+
+	inputVar := mux.Vars(r)["id"]
+	ownerID, err := strconv.Atoi(inputVar)
+	if err != nil {
+		var info InfoMessage
+		info.Code = 300
+		info.Message = "Can't convert owner ID to int"
+		json.NewEncoder(w).Encode(info)
+		return
+	}
+
+	item, status := OwnerGetAccounts(ownerID)
+
+	if status == -1 {
+		var info InfoMessage
+		info.Code = 500
+		info.Message = "Service error"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 404 {
+		var info InfoMessage
+		info.Code = 404
+		info.Message = "Owner not found"
+		json.NewEncoder(w).Encode(info)
+	} else if status == 200 {
+		json.NewEncoder(w).Encode(item)
+	}
 
 }
--- db.go
+++ db.go
@@ -760,3 +760,343 @@ func OwnerListItem() ([]Owner, int) {
 	return list, 200
 
 }
+
+func DepoAccountNewItem(item DepoAccount) (int, int) {
+
+	openDBConnection()
+
+	var depoAccountID int
+	var lastInsertId int64
+
+	res, err := database.Exec("INSERT INTO depo_accounts (owner_id, title, code, broker) VALUES (?, ?, ?, ?)", item.Owner, item.Title, item.Code, item.Broker)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return depoAccountID, -1
+	}
+
+	lastInsertId, err = res.LastInsertId()
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return depoAccountID, -1
+	}
+
+	depoAccountID = int(lastInsertId)
+
+	closeDBConnection()
+	return depoAccountID, 200
+
+}
+
+func DepoAccountUpdateItem(item DepoAccount) int {
+	openDBConnection()
+
+	rows, err := database.Query("SELECT id FROM depo_accounts WHERE id = ?", item.ID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return 404
+	}
+
+	_, err_upd := database.Exec("UPDATE depo_accounts SET owner_id = ?, title = ?, code = ?, broker = ? WHERE id = ?", item.Owner, item.Title, item.Code, item.Broker, item.ID)
+	if err_upd != nil {
+		log.Println(err_upd)
+		closeDBConnection()
+		return -1
+	}
+
+	closeDBConnection()
+	return 200
+
+}
+
+func DepoAccountGetItem(depoAccountID int) (DepoAccount, int) {
+
+	openDBConnection()
+
+	item := DepoAccount{}
+
+	rows, err := database.Query("SELECT id, owner_id, title, code, broker FROM depo_accounts WHERE id = ?", depoAccountID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return item, -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&item.ID, &item.Owner, &item.Title, &item.Code, &item.Broker)
+		if err != nil {
+			log.Println(err)
+			closeDBConnection()
+			return item, -1
+		}
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return item, 404
+	}
+
+	closeDBConnection()
+	return item, 200
+
+}
+
+func DepoAccountDeleteItem(DepoAccount int) int {
+	openDBConnection()
+
+	rows, err := database.Query("SELECT id FROM depo_accounts WHERE id = ?", DepoAccount)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return 404
+	}
+
+	_, err_upd := database.Exec("DELETE FROM depo_accounts WHERE id = ?", DepoAccount)
+	if err_upd != nil {
+		log.Println(err_upd)
+		closeDBConnection()
+		return -1
+	}
+
+	closeDBConnection()
+	return 200
+
+}
+
+func BankAccountNewItem(item BankAccount) (int, int) {
+
+	openDBConnection()
+
+	var depoAccountID int
+	var lastInsertId int64
+
+	res, err := database.Exec("INSERT INTO bank_accounts (owner_id, title, currency_id, code, broker) VALUES (?, ?, ?, ?, ?)", item.Owner, item.Title, item.Currency, item.Code, item.Broker)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return depoAccountID, -1
+	}
+
+	lastInsertId, err = res.LastInsertId()
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return depoAccountID, -1
+	}
+
+	depoAccountID = int(lastInsertId)
+
+	closeDBConnection()
+	return depoAccountID, 200
+
+}
+
+func BankAccountUpdateItem(item BankAccount) int {
+	openDBConnection()
+
+	rows, err := database.Query("SELECT id FROM bank_accounts WHERE id = ?", item.ID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return 404
+	}
+
+	_, err_upd := database.Exec("UPDATE bank_accounts SET owner_id = ?, title = ?, currency_id = ?, code = ?, broker = ? WHERE id = ?", item.Owner, item.Title, item.Currency, item.Code, item.Broker, item.ID)
+	if err_upd != nil {
+		log.Println(err_upd)
+		closeDBConnection()
+		return -1
+	}
+
+	closeDBConnection()
+	return 200
+
+}
+
+func BankAccountGetItem(bankAccountID int) (BankAccount, int) {
+
+	openDBConnection()
+
+	item := BankAccount{}
+
+	rows, err := database.Query("SELECT id, owner_id, title, currency_id, code, broker FROM bank_accounts WHERE id = ?", bankAccountID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return item, -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&item.ID, &item.Owner, &item.Title, &item.Currency, &item.Code, &item.Broker)
+		if err != nil {
+			log.Println(err)
+			closeDBConnection()
+			return item, -1
+		}
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return item, 404
+	}
+
+	closeDBConnection()
+	return item, 200
+
+}
+
+func BankAccountDeleteItem(bankAccountID int) int {
+	openDBConnection()
+
+	rows, err := database.Query("SELECT id FROM bank_accounts WHERE id = ?", bankAccountID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return 404
+	}
+
+	_, err_upd := database.Exec("DELETE FROM bank_accounts WHERE id = ?", bankAccountID)
+	if err_upd != nil {
+		log.Println(err_upd)
+		closeDBConnection()
+		return -1
+	}
+
+	closeDBConnection()
+	return 200
+
+}
+
+func OwnerGetAccounts(ownerID int) (OwnerAccounts, int) {
+	openDBConnection()
+
+	item := OwnerAccounts{}
+
+	rows, err := database.Query("SELECT id FROM owners WHERE id = ?", ownerID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return item, -1
+	}
+
+	count := 0
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&item.Owner)
+		if err != nil {
+			log.Println(err)
+			closeDBConnection()
+			return item, -1
+		}
+		count = count + 1
+	}
+
+	if count == 0 {
+		closeDBConnection()
+		return item, 404
+	}
+
+	// Depo Account List
+	list_da := []DepoAccount{}
+
+	rows_da, err := database.Query("SELECT id, owner_id, title, code, broker FROM depo_accounts WHERE owner_id = ?", ownerID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return item, -1
+	}
+
+	defer rows_da.Close()
+	for rows_da.Next() {
+		item_da := DepoAccount{}
+		err := rows_da.Scan(&item_da.ID, &item_da.Owner, &item_da.Title, &item_da.Code, &item_da.Broker)
+		if err != nil {
+			log.Println(err)
+			closeDBConnection()
+			return item, -1
+		}
+		list_da = append(list_da, item_da)
+	}
+
+	item.Depo = list_da
+
+	// Bank Account List
+	list_ba := []BankAccount{}
+
+	rows_ba, err := database.Query("SELECT id, owner_id, title, currency_id	, code, broker FROM bank_accounts WHERE owner_id = ?", ownerID)
+	if err != nil {
+		log.Println(err)
+		closeDBConnection()
+		return item, -1
+	}
+
+	defer rows_ba.Close()
+	for rows_ba.Next() {
+		item_ba := BankAccount{}
+		err := rows_ba.Scan(&item_ba.ID, &item_ba.Owner, &item_ba.Title, &item_ba.Currency, &item_ba.Code, &item_ba.Broker)
+		if err != nil {
+			log.Println(err)
+			closeDBConnection()
+			return item, -1
+		}
+		list_ba = append(list_ba, item_ba)
+	}
+
+	item.Bank = list_ba
+
+	closeDBConnection()
+	return item, 200
+
+}
